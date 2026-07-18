88b7f954b2fd8fc80f25a58239bc7b654a1c1c58
Aryan Hulawale <aryanhulawale2020@gmail.com>
2026-07-18 20:03:59 +0530

    Authentication setup done

--- src/app.js
+++ src/app.js
@@ -1,11 +1,40 @@
-import { configDotenv } from 'dotenv';
 import express from 'express';
-configDotenv();
+import helmet from 'helmet';
+import morgan from 'morgan';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import logger from './config/logger.js';
+import authRoutes from './routes/auth.routes.js';
+
 
 const app = express();
 
-app.use('/', (req, res) => {
-  res.send('Hello from aquizitions api');
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(morgan('combined', { stream: { write: (message) => logger.info(message.trim()) } }));
+
+
+
+
+app.use("/api/auth",authRoutes)
+
+
+
+app.get('/health', (req, res) => {
+  res.status(200).json({ status: 'OK', timestamp: new Date().toISOString(), uptime: process.uptime() });
 });
 
-export default app;
+app.get('/api', (req, res) => {
+  res.status(200).json({ message: 'Acquisitions API is running!' });
+});
+
+app.get('/', (req, res) => {
+  logger.info('Hello from Acquisitions!');
+
+  res.status(200).send('Hello from Acquisitions!');
+});
+
+export default app
